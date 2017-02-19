5f913deb018e704c1c0b61da73945bd2bf64612c
Tyler Lewis <Pinguinsan@gmail.com>
2017-02-18 21:02:54 -0500

    Working - chromebook-samus

--- firmware/test/LIN-Second/LIN-Second.ino
+++ firmware/test/LIN-Second/LIN-Second.ino
@@ -1,10 +1,3 @@
-/* This example code uses 2 different LIN LEDs of unknown type (acquired as 
-   samples).  You will have to modify this code to work with whatever LIN
-   devices you have.
-
-   TODO: make a Arduino LIN slave so the code works without modification.
- */
-
 #include "Arduino.h"
 #include "lin.h"
 #include "bitset.h"
@@ -12,9 +5,8 @@
 
 #define CS_PIN A0
 #define FAULT_TXE_PIN A1
-#define LIN_SERIAL_TX_PIN 18
+#define LIN_SERIAL_TX_PIN 1
 #define LIN_BAUD 19200L
-#define SERIAL_BAUD 115200L
 #define MESSAGE_LENGTH 8
 #define LIN_REVISION 2
 #define SEND_ID 0x35
@@ -23,41 +15,8 @@
 #define LINE_ENDING '\r'
 #define CHANGE_TIMEOUT 1000
 
-
 LIN *linController;
-void onFaultTxePinStateChange()
-{
-    if (digitalRead(FAULT_TXE_PIN)) {
-        onRisingEdgeOfFaultTxePin();
-    } else {
-        onFallingEdgeOfFaultTxePin();
-    }
-}
 
-void onRisingEdgeOfFaultTxePin()
-{
-    Serial.println("faultTxePin transitioned to the HIGH state");
-}
-
-void onFallingEdgeOfFaultTxePin()
-{
-    Serial.println("faultTxePin transitioned to the LOW state");
-}
-
-void receiveTest()
-{
-    uint8_t tempBuffer[MESSAGE_LENGTH]{0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
-    uint8_t receiveOkay = linController->receiveFrom(RECEIVE_ID, tempBuffer, MESSAGE_LENGTH, LIN_REVISION);
-    if (receiveOkay == 0) {
-        Serial.print("Receive timeout succeeded. ret = ");
-        Serial.println(receiveOkay, HEX);
-    }  else {
-        Serial.print("Receive timeout test failed. ret = ");
-        Serial.println(receiveOkay, HEX);
-    }
-}
-
-bool faultTxePinState{false};
 long long sendStartTime{millis()};
 long long sendEndTime{millis()};
 bool sendState{false};
@@ -65,18 +24,14 @@ LinMessage baseMessage{SEND_ID, LIN_REVISION};
 LinMessage allOnMessage{SEND_ID, LIN_REVISION};
 
 void setup() {
-    linController = new LIN{Serial1, LIN_SERIAL_TX_PIN};
+    linController = new LIN{Serial, LIN_SERIAL_TX_PIN};
     linController->begin(LIN_BAUD);
-    Serial.begin(SERIAL_BAUD);
-  
+    
     pinMode(CS_PIN, OUTPUT);
     digitalWrite(CS_PIN, HIGH);
 
     pinMode(FAULT_TXE_PIN, INPUT_PULLUP);
-    faultTxePinState = digitalRead(FAULT_TXE_PIN);
-    Serial.print("initial faultTxePinState: ");
-    Serial.println(faultTxePinState ? "HIGH" : "LOW");
-
+    
     uint8_t *allOnMessageData{static_cast<uint8_t *>(calloc(MESSAGE_LENGTH, sizeof(uint8_t)))};
     allOnMessageData[0] = 0xFF;
     allOnMessageData[1] = 0x00;
@@ -88,8 +43,6 @@ void setup() {
     allOnMessageData[7] = 0x00;
     allOnMessage = LinMessage{SEND_ID, LIN_REVISION, MESSAGE_LENGTH, allOnMessageData};
     free(allOnMessageData);
-
-    
 }
 
 void loop() {
@@ -103,7 +56,8 @@ void loop() {
         }
         sendStartTime = millis();
     }
-    
+
+    /*
     int status{0};
     LinMessage linMessage{linController->receiveFrom(RECEIVE_ID, MESSAGE_LENGTH, LIN_REVISION, status)};
     if (status == 0xff) {
@@ -127,11 +81,10 @@ void loop() {
         }
         Serial.println("");
 
-        /*
-        char tempMessage[SMALL_BUFFER_SIZE];
-        linMessage.toString(tempMessage, SMALL_BUFFER_SIZE);
-        Serial.println(tempMessage);
-        */
+        //char tempMessage[SMALL_BUFFER_SIZE];
+        //linMessage.toString(tempMessage, SMALL_BUFFER_SIZE);
+        //Serial.println(tempMessage);
+        
     } else {
 
     }
@@ -163,7 +116,9 @@ void loop() {
             onFallingEdgeOfFaultTxePin();
         }
     }
+        */
     doImAliveBlink();
+
 }
 
 void doImAliveBlink()
